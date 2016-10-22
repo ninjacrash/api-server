fe62343485e00de83f27418f1c8c1d1cea184e84
Ubuntu <ubuntu@ip-10-0-0-48.ec2.internal>
2016-10-22 08:53:19 +0000

    save

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var twilio = require('twilio');
 
 app.get('/api', function(req, res) {
-  res.sendStatus(200);
+  var resp = new twilio.TwimlResponse();
+  console.log(req.query);
+  resp.say('test message');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 });
 
 var server = app.listen(3001, function() {
